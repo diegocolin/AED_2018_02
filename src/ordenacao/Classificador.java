6fc239572091f98a93a33debdbe0657215ccc229
ejust <ejust@MC-4233.furb.br>
2018-11-14 18:58:36 -0200

    Classe do professor

---
+++ src/ordenacao/Classificador.java
@@ -0,0 +1,21 @@
+
+package ordenacao;
+
+/**
+ *
+ * @author marcel
+ */
+public class Classificador{
+    
+    public static <E extends Comparable> void bubbleSort(E[] vetor){
+        
+    }
+    
+    public static <E extends Comparable> void quickSort(E[] vetor){
+        
+    }
+    
+    public static <E extends Comparable> void mergeSort(E[] vetor){
+        
+    }
+}
