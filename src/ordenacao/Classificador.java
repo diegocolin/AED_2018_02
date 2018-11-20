ec058d51c774a568d0e60ee7b8c487176367ee3a
diegocolin <diegocolinsoares@hotmail.com>
2018-11-20 18:57:12 -0200

    Classificador do professor

--- src/ordenacao/Classificador.java
+++ src/ordenacao/Classificador.java
@@ -1,63 +1,96 @@
 package ordenacao;
 
-import javafx.scene.input.KeyCode;
-
 /**
  *
  * @author marcel
  */
 public class Classificador {
 
-    public static <E extends Comparable> void bubbleSort(E[] vet) {
-        int trocas = 0;
-        for (int i = 0; i < vet.length - 1; i++) {
-            if (vet[i].compareTo(vet[i + 1]) > 0) {
-                E temp = vet[i];
-                vet[i] = vet[i + 1];
-                vet[i + 1] = temp;
-                trocas++;
+    public static <E extends Comparable> void bubbleSort(E[] vetor) {
+        int n = vetor.length - 1;
+        boolean troca;
+        for (int i = 0; i < n; i++) {
+            troca = false;
+            for (int j = n; j > i; j--) {
+                if ((vetor[j - 1]).compareTo(vetor[j]) > 0) {
+                    E temp = vetor[j - 1];
+                    vetor[j - 1] = vetor[j];
+                    vetor[j] = temp;
+                    troca = true;
+                }
+            }
+            if (!troca) {
+                return;
             }
         }
-        if (trocas != 0) {
-            bubbleSort(vet);
+    }
+
+    private static <E extends Comparable> E[] quickSort(E[] a, int left, int right) {
+        int i = left;
+        int j = right;
+        E x = a[left];
+
+        while (i <= j) {
+            while (a[i].compareTo(x) < 0) {
+                i++;
+            }
+            while (x.compareTo(a[j]) < 0) {
+                j--;
+            }
+            if (i <= j) {
+                E temp = a[i];
+                a[i] = a[j];
+                i++;
+                a[j] = temp;
+                j--;
+            }
         }
+        if (left < j) {
+            quickSort(a, left, j);
+        }
+        if (i < right) {
+            quickSort(a, i, right);
+        }
+        return a;
     }
 
     public static <E extends Comparable> void quickSort(E[] vetor) {
+        quickSort(vetor, 0, vetor.length - 1);
+    }
 
+    private static <E extends Comparable> E[] mergeSort(E[] a, int low, int height) {
+        int l = low;
+        int h = height;
+
+        if (l >= h) {
+            return a;
+        }
+
+        int mid = (l + h) / 2;
+
+        mergeSort(a, l, mid);
+        mergeSort(a, mid + 1, h);
+
+        int end_lo = mid;
+        int start_hi = mid + 1;
+        while ((l <= end_lo) && (start_hi <= h)) {
+            if (a[l].compareTo(a[start_hi]) < 0) {
+                l++;
+            } else {
+                E temp = a[start_hi];
+                for (int k = start_hi - 1; k >= l; k--) {
+                    a[k + 1] = a[k];
+                }
+                a[l] = temp;
+                l++;
+                end_lo++;
+                start_hi++;
+            }
+        }
+        return a;
     }
 
     public static <E extends Comparable> void mergeSort(E[] vetor) {
-        int low = 0, high = 0, pi;
-        for (int i = 1; i < vetor.length - 1; i++) {
-            low = 0;
-            high = vetor.length;
-            if (vetor[i].compareTo(low) < 0) {
-                low = i;
-            }
-            if (vetor[i].compareTo(high) > 0) {
-                high = i;
-            }
-        }
-        if (low < high) {
-            E pivot = vetor[high];
-            int i = (low - 1);
-            for (int j = low; j < high; j++) {
-                if (vetor[j].compareTo(pivot) <= 0) {
-                    i++;
-                    E temp = vetor[i];
-                    vetor[i] = vetor[j];
-                    vetor[j] = temp;
-                }
-            }
-            E temp = vetor[i + 1];
-            vetor[i + 1] = vetor[high];
-            vetor[high] = temp;
-
-            pi = i + 1;
-            //mergeSort(vetor);
-            //mergeSort(vetor);
-        }
+        mergeSort(vetor, 0, vetor.length - 1);
     }
-
 }
