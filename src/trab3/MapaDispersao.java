c8dbbb2e27384f2c0ffbb70686376da9b366e556
Diego Arndt <diegoarndt@outlook.com>
2018-11-28 19:18:35 -0200

    Add files via upload
    Chora

---
+++ src/trab3/MapaDispersao.java
@@ -0,0 +1,28 @@
+public class MapaDispersao<K,T>{
+
+    private Lista<K,T>[] tabela;
+    
+    public MapaDispersao(int qdt) {
+    }
+    
+    private int calcularHash(K chave){
+        return 0;
+    }
+    
+    public boolean inserir (K chave, T dado){
+        return true;
+    }
+    
+    public T remover(K chave){
+        return null;
+    }
+    
+    public T buscar(K chave){
+        return null;
+    }
+    
+    public int quantosElementos(){
+        return 0;
+    }
+    
+}
